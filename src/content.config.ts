1c6ea10713ada151848c4fa32209fbfedcf031f1
Anton Pogrebnjak <contact@pantonius.dev>
2024-12-26 13:58:47 +0100

    Marcification begins

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,16 +2,17 @@ import { defineCollection, z } from "astro:content";
 import { file } from "astro/loaders";
 import { parse as parseCSV } from "csv-parse/sync";
 
-const structure = defineCollection({
-    loader: file("src/data/structure.json"),
-    schema: z.object({
-        question: z.string(),
-        answers: z.record(z.array(z.string())),
-    }),
+const questionaire = defineCollection({
+    loader: file("src/data/questionaire.json"),
+    schema:
+        z.record(  // Form Answers
+            z.string(), // Answer String
+            z.array(z.number()) // Answer Value
+        )
 });
 
 const hsg = defineCollection({
-    loader: file("src/data/hochschulgruppen.csv", { parser: (text) => parseCSV(text, { columns: true, skip_empty_lines: true, delimiter: ','}) }),
+    loader: file("src/data/hochschulgruppen.csv", { parser: (text) => parseCSV(text, { columns: true, skip_empty_lines: true, delimiter: ',' }) }),
 });
 
-export const collections = { structure, hsg };
+export const collections = { questionaire, hsg };
